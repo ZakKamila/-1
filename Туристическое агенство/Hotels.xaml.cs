0f08638d7dc02c99c32db294781d2ec94ead25ef
kzakirova959@gmail.com <kzakirova959@gmail.com>
2021-11-01 20:38:26 +0300

    Добавьте файлы проекта.

---
+++ Туристическое агенство/Hotels.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Туристическое_агенство
+{
+    /// <summary>
+    /// Логика взаимодействия для Hotels.xaml
+    /// </summary>
+    public partial class Hotels : Page
+    {
+        public Hotels()
+        {
+            InitializeComponent();
+            DGridHotels.ItemsSource = ТурАгенствоEntities.GetContext().Отели.ToList();
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AppEdit());
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
